2f7e4ee985c6ef76a33af774ca7fb9d052022f79
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-04-26 10:09:15 -0300

    welckson valentim - build: ProfileProvider.js, cria e configura provider

---
+++ project/src/Context/ProfileProvider.js
@@ -0,0 +1,11 @@
+import ProfileContext from "./ProfileContext";
+
+function ProfileProvider({ children }) {
+  return (
+    <ProfileContext.Provider>
+    { children }
+  </ProfileContext.Provider>
+  )
+}
+
+export default ProfileProvider;
