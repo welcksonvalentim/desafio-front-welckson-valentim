da1f9e3d7a39b977bb59449f1641d4d0861c1ef3
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-04-27 09:32:13 -0300

    welckson valentim - style: Login, estiliza componente Login

--- project/src/Pages/Login.js
+++ project/src/Pages/Login.js
@@ -1,43 +1,31 @@
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import '../App.css';
 
 function Login() {
   return (
     <main>
       <Header />
-        <form>
-          <label htmlFor="email">
+        <form className='login'>
+          <label htmlFor='email'>
             <h4>Email</h4>
             <input
-              className="input-name"
-              type="text"
-              placeholder="Digite seu email"
-              name="email"
+              type='text'
+              placeholder='Digite seu email'
+              name='email'
               required
-              value="Email"
             />
           </label>
-          <label htmlFor="password">
+          <label htmlFor='password'>
             <h4>Password</h4>
             <input
-              className="input-password"
-              type="text"
-              placeholder="Digite sua senha"
-              name="password"
+              type='text'
+              placeholder='Digite sua senha'
+              name='password'
               required
-              value="Password"
             />
           </label>
-          <label htmlFor="login-submit-button">
-            <input
-              className="button-input"
-              type="button"
-              name="buttonSubmit"
-              required
-              value="Logar"
-              onClick
-            />
-          </label>
+          <button>Logar</button>
         </form>
       <Footer />
     </main>
