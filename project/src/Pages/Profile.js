40541d7bbc270ab783d179de9e4c962a93a3cb8a
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-04-28 09:51:47 -0300

    welckson valenitm - feat:Profile, desenvolve profile para renderizar informações do usuario

--- project/src/Pages/Profile.js
+++ project/src/Pages/Profile.js
@@ -1,24 +1,52 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
+import '../Mock/mockAPI.json';
 import '../App.css';
 
 function Profile() {
-  const emailLogin = JSON.parse(localStorage.getItem('user'));
+  const [user, setUser] = useState({});
+
   const navigate = useNavigate();
   const handleClickExit = () => {
     localStorage.clear();
     navigate('/');
   };
 
+  useEffect(() => {
+    async function findUsers() {
+      const emailLogin = await JSON.parse(localStorage.getItem('user'));
+      const mockAPI = [
+        {
+          email: "admin@admin.com.br",
+          name: "admin",
+          age: 25,
+          city: "BH"
+        },
+        {
+          email: "validate@validate.com.br",
+          name: "validate",
+          age: 30,
+          city: "SP"
+        }
+      ];
+      const findUserIndb = mockAPI.find((user) => user.email === emailLogin.email);
+      setUser(findUserIndb);
+    }
+    findUsers();
+  }, []);
+
+
   return (
     <section>
       <Header />
+      { user ? (
       <section className="settingSectionPerfil">
-        <h4 className="settingSectionPerfilTitle">
-          {emailLogin !== null && emailLogin.email}
-        </h4>
+        <h4>{user.email}</h4>
+        <h4>{user.name}</h4>
+        <h4>{user.age}</h4>
+        <h4>{user.city}</h4>
         <button
           type="button"
           onClick={ handleClickExit }
@@ -27,6 +55,7 @@ function Profile() {
           Sair
         </button>
       </section>
+      ) : <h4>Usuário não cadastrado</h4>}
       <Footer />
     </section>
   );
