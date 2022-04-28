17312314a2cf5b57366a39c637a369aab4be33f5
WelcksonValentim-Turma15-TriboB <wellvalentim@hotmail.com>
2022-04-28 07:34:36 -0300

    welckson valenitm - build:Login, adiciona lógica para salvar informação de email no localStorage

--- project/src/Pages/Login.js
+++ project/src/Pages/Login.js
@@ -1,31 +1,55 @@
+import React, { useState } from 'react';
 import Header from '../Components/Header';
 import Footer from '../Components/Footer';
 import '../App.css';
 
 function Login() {
+
+  const [login, setLogin] = useState({
+    email: '',
+  });
+
+  const handleInputChange = ({ target: { name, value } }) => {
+    setLogin({ ...login, [name]: value });
+  };
+
+  const handleClick = (event) => {
+    localStorage.setItem('user', JSON.stringify({ email: login.email }));
+    window.location.href.push('/perfil');
+  };
+
   return (
     <main>
       <Header />
-        <form className='login'>
+        <form className='login' onSubmit={ (event) => handleClick(event) }>
           <label htmlFor='email'>
             <h4>Email</h4>
             <input
-              type='text'
+              type='email'
               placeholder='Digite seu email'
               name='email'
+              value={ login.email }
+              onChange={ handleInputChange }
               required
             />
           </label>
           <label htmlFor='password'>
             <h4>Password</h4>
             <input
-              type='text'
+              type='password'
               placeholder='Digite sua senha'
               name='password'
               required
             />
           </label>
-          <button>Logar</button>
+          <label htmlFor='button'>
+            <input
+              type='submit'
+              name='button'
+              value='Logar'
+              required
+            />
+          </label>
         </form>
       <Footer />
     </main>
